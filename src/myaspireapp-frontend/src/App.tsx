95f787cfcd8cbf59b336acc3aa796fa13fa1f158
kokutaro <h_kohara@me.com>
2025-06-29 12:14:33 +0900

    test: intentionally fails tests

--- src/myaspireapp-frontend/src/App.tsx
+++ src/myaspireapp-frontend/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={increment}>count is {count}</button>
+        <button onClick={increment}>count is a {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
